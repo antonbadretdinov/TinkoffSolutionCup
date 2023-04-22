c81f2c5131a3a430762f34931f9fa7c29089601f
Anton <antonbad03@gmail.com>
2023-04-22 17:27:28 +0300

    final commit

--- app/src/main/java/com/example/tinkoffsolutioncupfinal/MainActivity.kt
+++ app/src/main/java/com/example/tinkoffsolutioncupfinal/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tinkoffsolutioncupfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tinkoffsolutioncupfinal.ui.Header
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
